94809fd680e7792a80fd07eca39abe6559b17d81
Thomas <thomas@craftacademy.se>
2021-03-12 21:55:19 +0100

    removes generated test APIs folders

--- my-test/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const resourceController = require('../controllers/resourceController')
-
-/* Create routes for each controller in your application. */
-router
-  .get('/resource', resourceController.index)
-
-module.exports = router
--- testing/specs/factories/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (factory, Models) => {
-  const normalizedPath = require('path').join(__dirname, '.')
-  require('fs').readdirSync(normalizedPath).forEach(file => {
-    if (file !== 'index.js') {
-      require(`./${file}`)(factory, Models)
-    }
-  })
-}
